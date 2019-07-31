ced692b0b60294fcc5e2aac1642b8a412ea0f5b6
Konstantin <k0nstantinkolos88@gmail.com>
2019-07-31 20:37:40 +0300

    Small error in the multiboot block
    Small error in the multiboot block (see screenshot).

--- admin/views/phocadownloadmanager/tmpl/default_upload.php
+++ admin/views/phocadownloadmanager/tmpl/default_upload.php
@@ -11,7 +11,7 @@ echo '<div id="'.$this->t['c'].'-upload" class="ph-in">';
 echo '<div id="upload-noflash" class="actions">';
 echo '<form action="'. $this->t['su_url'] .'" id="uploadFormU" method="post" enctype="multipart/form-data">';
 if ($this->t['ftp']) { echo PhocaDownloadFileUpload::renderFTPaccess();}  
-echo '<div class="control-label ph-head-form">'. JText::_( $this->t['l'].'_UPLOAD_FILE' ).' [ '. JText::_( $this->t['l'].'_MAX_SIZE' ).':&nbsp;'.$this->t['uploadmaxsizeread'].'] </div>';
+echo '<div class="control-label ph-head-form">'. JText::_( $this->t['l'].'_UPLOAD_FILE' ).' ['. JText::_( $this->t['l'].'_MAX_SIZE' ).':&nbsp;'.$this->t['uploadmaxsizeread'].'] </div>';
 echo $this->t['su_output'];
 echo '</form>';
 echo '</div>';
